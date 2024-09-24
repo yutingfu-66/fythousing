2efec7eff6a111e99acd9336f7ad9699ce72e0fd
yutingfu-66 <fu6yuting@gmail.com>
2024-09-24 13:00:02 +0800

    initial change

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import seaborn as sns
 sns.set()
 
 
-st.title('World Cites')
-df = pd.read_csv('worldcities.csv')
+st.title('California Housing Data')
+df = pd.read_csv('housing.csv')
 
 # note that you have to use 0.0 and 40.0 given that the data type of population is float
-population_filter = st.slider('Minimal Population (Millions):', 0.0, 40.0, 3.6)  # min, max, default
+price_filter = st.slider('Minimal Median House Price:', 0, 500001, 20000)  # min, max, default
 
 # create a multi select
-capital_filter = st.sidebar.multiselect(
-     'Capital Selector',
-     df.capital.unique(),  # options
-     df.capital.unique())  # defaults
+location_filter = st.sidebar.multiselect(
+     'Choose the location type',
+     df.ocean_proximity.unique(),  # options
+     df.ocean_proximity.unique())  # defaults
 
 # create a input form
 form = st.sidebar.form("country_form")
@@ -24,24 +24,23 @@ form.form_submit_button("Apply")
 
 
 # filter by population
-df = df[df.population >= population_filter]
+df = df[df.median_house_value >= price_filter]
 
 # filter by capital
-df = df[df.capital.isin(capital_filter)]
+df = df[df.ocean_oproxmity.isin(location_filter)]
 
-if country_filter!='ALL':
-    df = df[df.country == country_filter]
 
 # show on map
-st.map(df)
+st.map(filtered_df)
 
-# show dataframe
-st.subheader('City Details:')
-st.write(df[['city', 'country', 'population']])
 
-# show the plot
-st.subheader('Total Population By Country')
-fig, ax = plt.subplots(figsize=(20, 5))
-pop_sum = df.groupby('country')['population'].sum()
-pop_sum.plot.bar(ax=ax)
+st.subheader('Median House Value')
+fig, ax = plt.subplots(figsize=(20, 20))
+ax.hist(filtered_df['median_house_value'], bin = 30)
+
+ax.set_title('Histogram of Median House Value', fontsize = 16)
+ax.set_xlable('Media House Value', fontsize = 12)
+ax.set_ylable('Frequency', fontsize = 12)
+
+
 st.pyplot(fig)
